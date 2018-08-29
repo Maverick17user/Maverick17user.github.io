c79caf049fe7700eee0d6f722b74ac6332298d53
Maxim Epifanov <maksim.epifanov@mail.ru>
2018-08-29 12:38:42 +0200

    footer alredy ready

---
+++ common.js
@@ -0,0 +1,12 @@
+// let clickEl = document.querySelector('a.contact');
+// $(document).ready(function () {
+//     $("a.contact").click(function (e) {
+//         e.preventDefault();
+//         setTimeout(() => {
+//             var elementClick = $(this).attr("href");
+//             var destination = $(elementClick).offset().top;
+//             $('body').animate({ scrollTop: destination }, 1000);
+//         }, 1000);
+//         return false; 
+//     });
+// });
