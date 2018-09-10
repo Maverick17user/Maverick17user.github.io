46adffca706a696c46a925bf71f3f55150852642
Maksim Epifanau <MEpifanau@gomel.iba.by>
2018-09-10 21:18:46 +0200

    preloader&scrollStyle&newAnime

---
+++ common.js
@@ -0,0 +1,18 @@
+setTimeout(function() {
+    $('html, body').css('overflowY', 'visible');
+},1100);
+
+setTimeout(function() {
+    var face = document.querySelector("section.skills");
+    var some = document.querySelectorAll("span.skill_description");
+    console.log(face);
+    face.onscroll = function() {
+        // some.forEach(element => {
+        //     $(element).css({
+        //         "-webkit-transform":"translate(100px,100px)",
+        //         "-ms-transform":"translate(100px,100px)",
+        //         "transform":"translate(100px,100px)"
+        //     })
+        // }) 
+    };
+}, 0);
